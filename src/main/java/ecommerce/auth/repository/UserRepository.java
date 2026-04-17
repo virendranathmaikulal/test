0ce96a74b1e66b3d8b2d43ba9b5637a88003e0ac
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-04-17 01:17:23 -0500

    Create UserRepository.java

---
+++ src/main/java/ecommerce/auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.ecommerce.auth.repository;
+
+import com.ecommerce.auth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
