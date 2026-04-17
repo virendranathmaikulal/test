dd07250834c063a598ca77d2248c17817e4d9a33
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-04-17 01:10:48 -0500

    Update ForgotPasswordRequest.java

--- src/main/java/ecommerce/auth/dto/request/ForgotPasswordRequest.java
+++ src/main/java/ecommerce/auth/dto/request/ForgotPasswordRequest.java
@@ -1,1 +1,16 @@
 
+package com.ecommerce.auth.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ForgotPasswordRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
+    private String email;
+}
