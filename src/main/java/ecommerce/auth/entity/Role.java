b88352c7cde80bb6d7ae8bad760b9bcb1ef72778
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-04-17 01:13:58 -0500

    Create Role.java

---
+++ src/main/java/ecommerce/auth/entity/Role.java
@@ -0,0 +1,30 @@
+package com.ecommerce.auth.entity;
+
+import com.ecommerce.auth.enums.RoleName;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "role_id")
+    private UUID roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role_name", nullable = false, unique = true, length = 20)
+    private RoleName roleName;
+
+    public Role(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
