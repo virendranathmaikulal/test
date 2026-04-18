27b875a8d7830ec48272354cc16e91028765dd2a
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-04-17 19:39:13 -0500

    Update ResetPasswordRequest.java

--- src/main/java/ecommerce/auth/dto/request/ResetPasswordRequest.java
+++ src/main/java/ecommerce/auth/dto/request/ResetPasswordRequest.java
@@ -1,5 +1,6 @@
 package com.ecommerce.auth.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -10,9 +11,12 @@ import lombok.Setter;
 @Setter
 public class ResetPasswordRequest {
 
+    @JsonProperty("reset_token")
     @NotBlank(message = "Reset token is required")
+    @Size(max = 64, message = "Reset token must not exceed 64 characters")
     private String resetToken;
 
+    @JsonProperty("new_password")
     @NotBlank(message = "New password is required")
     @Size(min = 8, max = 64, message = "Password must be between 8 and 64 characters")
     @Pattern(
