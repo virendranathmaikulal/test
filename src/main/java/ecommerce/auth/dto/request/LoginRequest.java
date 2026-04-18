5383a7016e457a7585555bd408cd15935c250fc1
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-04-17 19:37:48 -0500

    Update LoginRequest.java

--- src/main/java/ecommerce/auth/dto/request/LoginRequest.java
+++ src/main/java/ecommerce/auth/dto/request/LoginRequest.java
@@ -2,6 +2,7 @@ package com.ecommerce.auth.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,10 @@ public class LoginRequest {
 
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
+    @Size(max = 150, message = "Email must not exceed 150 characters")
     private String email;
 
     @NotBlank(message = "Password is required")
+    @Size(max = 64, message = "Password must not exceed 64 characters")
     private String password;
 }
