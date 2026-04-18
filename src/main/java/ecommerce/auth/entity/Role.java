e55bb7b8bf48c1f74cc30674b105c076d0909f34
virendranathmaikulal <virendranathmaikulal@gmail.com>
2026-04-17 19:40:41 -0500

    Update Role.java

--- src/main/java/ecommerce/auth/entity/Role.java
+++ src/main/java/ecommerce/auth/entity/Role.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Entity
@@ -26,5 +27,18 @@ public class Role {
 
     public Role(RoleName roleName) {
         this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return roleName == role.roleName;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleName);
     }
 }
